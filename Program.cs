9fe19d234cb32e977567dd9af8fffe7c7a27b293
Avet1k <xavetikx@gmail.com>
2023-10-11 16:44:43 +0300

    Added Enclosure

--- Program.cs
+++ Program.cs
@@ -3,13 +3,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Another test SSH push");
+        
     }
 }
 
 abstract class Animal
 {
-    public Animal(string name, string sex, string sound)
+    public Animal(string name, char sex, string sound)
     {
         Name = name;
         Sex = sex;
@@ -17,7 +17,7 @@ abstract class Animal
     }
     
     public string Name { get; }
-    public string Sex { get; }
+    public char Sex { get; }
     public string Sound { get; }
 
     public virtual void MakeSound()
@@ -31,21 +31,63 @@ abstract class Animal
 
 class Wolf : Animal
 {
-    public Wolf(string sex) : base("Волк", sex, "Ауф") { }
+    public Wolf(char sex) : base("Волк", sex, "Ауф") { }
 }
 
 class Capybara : Animal
 {
-    public Capybara(string sex) : base("Капибара", sex, string.Empty) { }
+    public Capybara(char sex) : base("Капибара", sex, string.Empty) { }
 
     public override void MakeSound()
     {
         base.MakeSound();
-        Console.WriteLine($"{Name} лишь тихо лежит и смотрит на вас.");
+        Console.WriteLine($"{Name} лишь тихо лежит и смотрит.");
     }
 }
 
 class Frog : Animal
 {
-    public Frog(string sex) : base("Лягушка", sex, "Ква-ква") { }
+    public Frog(char sex) : base("Лягушка", sex, "Ква-ква") { }
+}
+
+class Enclosure
+{
+    private List<Animal> _animals;
+    private string _description;
+    private int _malesCount;
+    private int _femalesCount;
+    
+    public Enclosure(int capacity, string description)
+    {
+        _animals = new List<Animal>(capacity);
+        _description = description;
+    }
+
+    public void ShowDescription()
+    {
+        Console.WriteLine();  //TODO
+    }
+
+    public void AddAnimal(Animal animal)
+    {
+        char maleSymbol = 'M';
+        char femaleSymbol = 'F';
+        
+        _animals.Add(animal);
+
+        if (animal.Sex == maleSymbol)
+            _malesCount++;
+        else if (animal.Sex == femaleSymbol)
+            _femalesCount++;
+    }
+}
+
+class Zoo
+{
+    private Enclosure[] _enclosures;
+
+    public Zoo()
+    {
+        _enclosures = new Enclosure[4];
+    }
 }

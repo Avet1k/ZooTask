f8046c35d7e731f2222b054d9ba188d2517b96b9
Avet1k <xavetikx@gmail.com>
2023-10-11 12:04:21 +0300

    Animals added

--- Program.cs
+++ Program.cs
@@ -6,3 +6,46 @@ class Program
         Console.WriteLine("Another test SSH push");
     }
 }
+
+abstract class Animal
+{
+    public Animal(string name, string sex, string sound)
+    {
+        Name = name;
+        Sex = sex;
+        Sound = sound;
+    }
+    
+    public string Name { get; }
+    public string Sex { get; }
+    public string Sound { get; }
+
+    public virtual void MakeSound()
+    {
+        if (Sound != string.Empty)
+            Console.WriteLine($"{Name} говорит {Sound}!");
+        else
+            Console.WriteLine($"{Name} ничего не говорит.");
+    }
+}
+
+class Wolf : Animal
+{
+    public Wolf(string sex) : base("Волк", sex, "Ауф") { }
+}
+
+class Capybara : Animal
+{
+    public Capybara(string sex) : base("Капибара", sex, string.Empty) { }
+
+    public override void MakeSound()
+    {
+        base.MakeSound();
+        Console.WriteLine($"{Name} лишь тихо лежит и смотрит на вас.");
+    }
+}
+
+class Frog : Animal
+{
+    public Frog(string sex) : base("Лягушка", sex, "Ква-ква") { }
+}
